80cf9e98c866033df155d1c4db63fc28b6226f59
Obrad Kostov <obrad.kostovelfak.rs>
2026-06-14 21:09:09 +0200

    Added Jwt strategy

--- src/auth/guards/roles.guard.ts
+++ src/auth/guards/roles.guard.ts
@@ -3,11 +3,15 @@ import { Reflector } from "@nestjs/core";
 import { Observable } from "rxjs";
 
 @Injectable()
-export class RolesGuard implements CanActivate{
-    constructor(private reflector:Reflector){}
-    canActivate(context: ExecutionContext): boolean{
-        const roles=this.reflector.get<string[]>('roles',context.getHandler());
-        if(!roles)return true;
+export class RolesGuard implements CanActivate {
+    constructor(private reflector: Reflector) { }
+    canActivate(context: ExecutionContext): boolean {
+        const roles = this.reflector.get<string[]>('roles', context.getHandler());
+        if (!roles) return true;
 
+        const request = context.switchToHttp().getRequest();
+        const user = request.user;
+        if (!user) return false;
+        return roles.includes(user.role);
     }
 }
