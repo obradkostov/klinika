eb362bea3b5e1e6774cce186db776292ded76725
Obrad Kostov <obrad.kostovelfak.rs>
2026-06-15 22:40:54 +0200

    Add register component and navigation

--- klinika-app/src/app/components/login/login.component.ts
+++ klinika-app/src/app/components/login/login.component.ts
@@ -2,12 +2,12 @@ import { CommonModule } from "@angular/common";
 import { Component } from "@angular/core";
 import { FormsModule } from '@angular/forms';
 import { AuthService } from "../../services/auth.service";
-import { Router } from "@angular/router";
+import { Router, RouterLink } from "@angular/router";
 
 @Component({
     selector: 'app-login',
     standalone: true,
-    imports: [FormsModule, CommonModule],
+    imports: [FormsModule, CommonModule, RouterLink],
     templateUrl: './login.component.html',
     styleUrl: './login.css'
 })
--- klinika-app/src/app/components/register/register.ts
+++ klinika-app/src/app/components/register/register.ts
@@ -1,9 +1,29 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { AuthService } from '../../services/auth.service';
+import { Router, RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-register',
-  imports: [],
+  imports: [FormsModule, CommonModule, RouterLink],
   templateUrl: './register.html',
   styleUrl: './register.css',
 })
-export class Register {}
+export class RegisterComponent {
+  email='';
+  password='';
+  role='PATIENT';
+  error='';
+  constructor(private authService:AuthService,private router:Router){}
+  register(){
+    this.authService.register(this.email,this.password,this.role).subscribe({
+      next:()=>{
+        this.router.navigate(['/login']);
+      },
+      error:()=>{
+        this.error="Greska pri registraciji";
+      }
+    });
+  }
+}
