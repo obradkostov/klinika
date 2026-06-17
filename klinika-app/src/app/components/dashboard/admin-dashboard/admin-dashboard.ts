5fedc2ba1a143cd779038c010d588410dc7a80ca
Obrad Kostov <obrad.kostovelfak.rs>
2026-06-17 18:25:55 +0200

    Add doctors list on admin dashboard

--- klinika-app/src/app/components/dashboard/admin-dashboard/admin-dashboard.ts
+++ klinika-app/src/app/components/dashboard/admin-dashboard/admin-dashboard.ts
@@ -1,18 +1,36 @@
 import { Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { Router } from '@angular/router';
+import { Doctors } from '../../../services/doctors';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-admin-dashboard',
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './admin-dashboard.html',
   styleUrl: './admin-dashboard.css',
 })
 export class AdminDashboard implements OnInit{
-   constructor(private authService:AuthService,private router:Router){}
-    ngOnInit(){}
-    logout(){
-        this.authService.logOut;
-        this.router.navigate(['/login']);
-    }
+  doctors:any[]=[];
+   constructor(
+    private authService: AuthService,
+    private router: Router,
+    private doctorsService: Doctors
+  ) {}
+
+  ngOnInit() {
+    this.doctorsService.getAll().subscribe({
+      next: (data) => {
+        this.doctors = data;
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+  }
+
+  logout() {
+    this.authService.logOut();
+    this.router.navigate(['/login']);
+  }
 }
