9603067448f03358a9d017e3fa632838d9c0799d
Obrad Kostov <obrad.kostovelfak.rs>
2026-06-21 19:55:38 +0200

    Add patients list to admin dashboard

--- klinika-app/src/app/components/dashboard/admin-dashboard/admin-dashboard.ts
+++ klinika-app/src/app/components/dashboard/admin-dashboard/admin-dashboard.ts
@@ -1,8 +1,9 @@
-import { Component, OnInit } from '@angular/core';
+import { ChangeDetectorRef, Component, OnInit } from '@angular/core';
 import { AuthService } from '../../../services/auth.service';
 import { Router, RouterLink } from '@angular/router';
 import { Doctors } from '../../../services/doctors';
 import { CommonModule } from '@angular/common';
+import { Patients } from '../../../services/patients';
 
 @Component({
   selector: 'app-admin-dashboard',
@@ -12,16 +13,30 @@ import { CommonModule } from '@angular/common';
 })
 export class AdminDashboard implements OnInit{
   doctors:any[]=[];
+  patients:any[]=[];
    constructor(
     private authService: AuthService,
     private router: Router,
-    private doctorsService: Doctors
+    private doctorsService: Doctors,
+    private patientsService:Patients,
+    private cdr: ChangeDetectorRef
+
   ) {}
 
   ngOnInit() {
     this.doctorsService.getAll().subscribe({
       next: (data) => {
         this.doctors = data;
+        this.cdr.markForCheck();
+      },
+      error: (err) => {
+        console.error(err);
+      }
+    });
+    this.patientsService.getAll().subscribe({
+      next: (data) => {
+        this.patients = data;
+        this.cdr.markForCheck();
       },
       error: (err) => {
         console.error(err);
